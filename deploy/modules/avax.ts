3ee41bd21966f2d08f51b79cfc0a1cc242232dc1
Ramesh Nair <ram@hiddentao.com>
2022-05-25 22:44:45 +0100

    fix: Dex contract key name for avax

--- deploy/modules/avax.ts
+++ deploy/modules/avax.ts
@@ -8,20 +8,20 @@ export const deployAvaxDex = async (ctx: Context = {} as Context) => {
 
   let dex: Contract = {} as Contract
 
-  if (!deployedAddressesToSave.AvaxDex) {
+  if (!deployedAddressesToSave.Dex) {
     await log.task(`Deploy Avalanche DEX`, async parentTask => {
       dex = await deployContract(ctx, 'AvaxDex', [])
 
       await parentTask.log(`Deployed at ${dex.address}`)
 
       if (expectedDeployedAddresses) {
-        assertSameAddress(dex.address, expectedDeployedAddresses.AvaxDex, 'AvaxDex')
+        assertSameAddress(dex.address, expectedDeployedAddresses.Dex, 'Dex')
       }
 
-      deployedAddressesToSave.AvaxDex = dex.address
+      deployedAddressesToSave.Dex = dex.address
     })
   } else {
-    dex = await getContractAt('IDex', deployedAddressesToSave.AvaxDex)
+    dex = await getContractAt('IDex', deployedAddressesToSave.Dex)
   }
 
   return dex
